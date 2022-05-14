ad9e00435a55626088a63738ae85308d114d01a4
Lucas Otávio Leme Silva <49615892+lucaspvpa@users.noreply.github.com>
2022-05-14 03:13:06 -0300

    Add files via upload

--- PlayFair.py
+++ PlayFair.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
 import re
 import string
 
@@ -16,7 +22,6 @@ def criptografar(chave, texto):
             pass
         else:
             lista_letras_nao_repetidas.append(letra)
-    
     matriz = [lista_letras_nao_repetidas[0:5],
               lista_letras_nao_repetidas[5:10],
               lista_letras_nao_repetidas[10:15],
@@ -72,6 +77,70 @@ def criptografar(chave, texto):
         texto_criptografado.append(matriz[linha_1][coluna_1])
     return ''.join(texto_criptografado)
 
+def descriptografar(chave, texto):
+    chave = chave.replace('j', 'i')
+    lista_letras_nao_repetidas = []
+    lista_alfabeto = list(string.ascii_lowercase)
+    lista_alfabeto.remove('j')
+    for letra in chave:
+        if letra in lista_letras_nao_repetidas:
+            pass
+        else:
+            lista_letras_nao_repetidas.append(letra)
+    for letra in lista_alfabeto:
+        if letra in lista_letras_nao_repetidas:
+            pass
+        else:
+            lista_letras_nao_repetidas.append(letra)
+    matriz = [lista_letras_nao_repetidas[0:5],
+              lista_letras_nao_repetidas[5:10],
+              lista_letras_nao_repetidas[10:15],
+              lista_letras_nao_repetidas[15:20],
+              lista_letras_nao_repetidas[20:25]]
+    lista_palavras_duplas = []
+    lista_texto = []
+    for i, letra in enumerate(texto):
+        lista_texto.append(letra)
+    x = 0
+    for i,letra in enumerate(lista_texto):
+        if x == 1:
+            x = 0
+            lista_palavras_duplas.append(lista_texto[i-1:i+1])
+        else:
+            x += 1
+    texto_criptografado = []
+    for letras in lista_palavras_duplas:
+        for i, item in enumerate(matriz):
+            if letras[0] in item and letras[1] in item:
+                index = item.index(letras[1])
+                coluna_1 = index - 1
+                coluna_2 = index - 2
+                linha_1 = i
+                linha_2 = i
+                break
+            try:
+                coluna_1 = item.index(letras[0])
+            except:
+                pass
+            if letras[1] in item:
+                linha_1 = i
+            try:
+                coluna_2 = item.index(letras[1])
+            except:
+                pass
+            if letras[0] in item:
+                linha_2 = i
+        if coluna_2 == coluna_1:
+            linha_2 -= 1
+            linha_1 -= 1
+            if linha_1 < 0:
+                linha_1 = 4
+            if linha_2 < 0:
+                linha_2 = 4
+        texto_criptografado.append(matriz[linha_2][coluna_2])
+        texto_criptografado.append(matriz[linha_1][coluna_1])
+    return ''.join(texto_criptografado)
+
 def verificar_apenas_letra(texto):
     resultado = False
     pattern = '^[a-z]*$'
@@ -89,7 +158,7 @@ def verificar_apenas_letra(texto):
     return resultado
     
 while(1):
-    escolha = int(input('Você deseja 1 - criptografar ou 2 - Sair\n'))
+    escolha = int(input('Você deseja 1 - criptografar, 2- Descriptografar ou 3 - Sair\n'))
     if escolha == 1:
         palavra_chave = input('Insira sua chave para criptografar(Deve conter apenas letras sem espaço):\n').lower()
         resultado = verificar_apenas_letra(palavra_chave)
@@ -100,6 +169,22 @@ while(1):
                 palavra_criptografada = criptografar(palavra_chave, palavra_criptografar)
                 print(palavra_criptografada)
     elif escolha == 2:
+        palavra_chave = input('Insira sua chave para descriptografar(Deve conter apenas letras sem espaço):\n').lower()
+        resultado = verificar_apenas_letra(palavra_chave)
+        if resultado:
+            palavra_descriptografar = input('Insira uma palavra para ser descriptografada:\n').lower()
+            resultado = verificar_apenas_letra(palavra_descriptografar)
+            if resultado:
+                palavra_descriptografada = descriptografar(palavra_chave, palavra_descriptografar)
+                print(palavra_descriptografada)
+    elif escolha == 3:
         break
     else:
         print('Essá fucao não está disponível')
+
+
+# In[ ]:
+
+
+
+
